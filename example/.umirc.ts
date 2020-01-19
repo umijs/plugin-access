e96bda97474e03298bdda6437ecca0a39c23833b
wtzeng1 <wtzeng1@gmail.com>
2020-01-19 18:24:38 +0800

    feat: child route inherits from parent route

--- example/.umirc.ts
+++ example/.umirc.ts
@@ -4,6 +4,12 @@ import { IConfig } from 'umi-types';
 export default {
   routes: [
     { path: '/', component: './index', access: 'readArticle' },
+    {
+      path: '/update',
+      component: './index',
+      access: 'updateArticle',
+      routes: [{ path: 'test', component: './index' }],
+    },
   ],
   plugins: [
     join(__dirname, '..', require('../package').main || 'index.js'),
